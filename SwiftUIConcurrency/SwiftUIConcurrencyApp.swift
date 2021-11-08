baaa80d3af0c2fda09a20a81d55e4163825e9c76
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-11-08 13:38:28 +0530

    Initial Commit

---
+++ SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by KANISHK VIJAYWARGIYA on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
